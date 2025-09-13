4353a033b70f89a6b1c7e935448f420aebac9af5
mironiukia <ilia.mironyuk21@gmail.com>
2025-09-13 14:01:53 +0200

    resolve conflict

--- internal/scheduler/sheduler.go
+++ internal/scheduler/sheduler.go
@@ -7,11 +7,11 @@ import (
 )
 
 type Scheduler struct {
-	service task.TasksService
+	service *task.TasksService
 	ticker  *time.Ticker
 }
 
-func NewScheduler(service task.TasksService, interval time.Duration) *Scheduler {
+func NewScheduler(service *task.TasksService, interval time.Duration) *Scheduler {
 	return &Scheduler{
 		service: service,
 		ticker:  time.NewTicker(interval),
